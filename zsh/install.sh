2dbe465a3522c7a7acbf560030b92ad16d89eff4
LB <lb@vxider.com>
2017-12-24 22:08:39 +0800

    fixing bugs

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -r ~/.dotfiles/zsh/powerlevel9k ~/.oh-my-zsh/custom/themes/
+cp -r ~/.dotfiles/zsh/powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 echo 'export DEFAULT_USER="Vxider"' > ~/.zshrc.local
 echo 'Please set DEFAULT_USER in ~/.zshrc.local'
