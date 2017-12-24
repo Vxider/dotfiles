780bce0e3beab9e79b51e9f9d9e5f9ba47e95c28
LB <lb@vxider.com>
2017-12-24 21:22:32 +0800

    add -sf to ln

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 echo 'export DEFAULT_USER="Vxider"' > ~/.zshrc.local
 echo 'Please set DEFAULT_USER in ~/.zshrc.local'
 echo 'Installed zshrc configuration successfully!'
