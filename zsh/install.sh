aaec31811ad1f7ce794a4ed4b0cb9f345b33fdba
LB <lb@vxider.com>
2017-12-24 21:49:54 +0800

    add powerlevel9k

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cp -r powerlevel9k ~/.oh-my-zsh/custom/themes/
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 echo 'export DEFAULT_USER="Vxider"' > ~/.zshrc.local
 echo 'Please set DEFAULT_USER in ~/.zshrc.local'
