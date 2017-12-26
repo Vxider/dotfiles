7a81b1b2ba3e6296a89c2abde29a421db79e4b3d
LB <lb@vxider.com>
2017-12-26 20:35:57 +0800

    add plugins

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 cp -r ~/.dotfiles/zsh/powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 echo 'export DEFAULT_USER="Vxider"' >> ~/.zshrc.local
