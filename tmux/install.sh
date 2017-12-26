2bee03829178b02e1af5eaeca182f8b8d74aace9
LB <lb@vxider.com>
2017-12-26 17:16:24 +0800

    fixing bugs

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,6 +3,5 @@
 cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -sf .tmux/.tmux.conf
-ln -sf ~/.dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
 
 echo 'Installed the Tmux configuration successfully!'
