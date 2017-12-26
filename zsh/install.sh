4a055621d81efe8d7361150eae1fb2801b259f6f
LB <lb@vxider.com>
2017-12-26 20:43:39 +0800

    add zsh and ohmyzsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+# install zsh
+sudo yum -y install zsh
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 # install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
 # install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 cp -r ~/.dotfiles/zsh/powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
-echo 'export DEFAULT_USER="Vxider"' >> ~/.zshrc.local
+echo 'export DEFAULT_USER=""' >> ~/.zshrc.local
 echo 'Please set DEFAULT_USER in ~/.zshrc.local'
 echo 'Installed zshrc configuration successfully!'
