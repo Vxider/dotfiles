78f83efd65a0fee2923bff3d7b7256b0e82538f6
LB <lb@vxider.com>
2017-12-27 19:12:34 +0800

    fixing bugs

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,7 +4,7 @@
 sudo yum -y install zsh
 
 # install oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
@@ -14,6 +14,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 cp -r ~/.dotfiles/zsh/powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
+
 echo 'export DEFAULT_USER=""' >> ~/.zshrc.local
 echo 'Please set DEFAULT_USER in ~/.zshrc.local'
 echo 'Installed zshrc configuration successfully!'
