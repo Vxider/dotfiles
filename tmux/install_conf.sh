b18db607fda115f752be23049796949430987ec0
LB <lb@vxider.com>
2018-02-27 11:06:48 +0800

    change install_conf

--- tmux/install_conf.sh
+++ tmux/install_conf.sh
@@ -2,6 +2,7 @@
 
 cd
 git clone https://github.com/gpakosz/.tmux.git
-ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s -f .tmux/.tmux.conf
+ln -sf ~/.dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
 
 echo 'Installed the Tmux configuration successfully!'
