e74fc602b916253362ca15dccb42379082517115
Vxider <lb@vxider.com>
2019-07-06 12:13:37 +0800

    update

--- tmux/install_conf.sh
+++ tmux/install_conf.sh
@@ -2,6 +2,7 @@
 
 cd
 git clone https://github.com/gpakosz/.tmux.git
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s -f .tmux/.tmux.conf
 ln -sf ~/.dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
 
